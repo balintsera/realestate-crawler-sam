b7f9718b40340243e30245bb5b1f3ea5624b39c4
Bálint Séra <balint.sera@redsteedstudios.com>
2018-12-29 15:19:49 +0100

    add user agents

--- crawler/src/service/dom-crawler.js
+++ crawler/src/service/dom-crawler.js
@@ -10,6 +10,13 @@ class DOMCrawler {
     this.urls = urls
     const _self = this
     this.crawler = new Crawler({
+      rotateUA: true,
+      userAgent: [
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.1 Safari/605.1.15',
+        'Mozilla/5.0 (Linux; U; Android 2.2) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
+        'Mozilla/5.0 (Windows NT 5.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1',
+      ],
       maxConnections: 10,
       // This will be called for each crawled page
       callback: function (error, res, done) {
