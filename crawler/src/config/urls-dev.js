b1f34b641e4ffde894c142c2130d7414c5caa689
Bálint Séra <balint.sera@redsteedstudios.com>
2019-04-07 10:14:16 +0200

    Add ingatlannet image

--- crawler/src/config/urls-dev.js
+++ crawler/src/config/urls-dev.js
@@ -2,6 +2,43 @@ const RealEstate = require('../model/real-estate')
 
 const urls = [
   {
+    uri: 'https://www.ingatlannet.hu/kiadó/ház/Szeged',
+    parentSelector: '.row.mb-md-3',
+    selectors: [
+      {
+        field: 'address',
+        selector: '.item-desc p b',
+        methods: {'text': [], 'trim': []} // property name: method name, value array: paramters for that method
+      },
+      {
+        field: 'price',
+        selector: '.item-price h5',
+        methods: {'text': [], 'trim': [] }
+      },
+      {
+        field: 'size',
+        selector: '.list-inline li:nth-child(1)',
+        methods: {'text': [], 'trim': []}
+      },
+      {
+        field: 'roomCount',
+        selector: '.list-inline li:nth-child(3)',
+        methods: {'text': [], 'trim': []}
+      },
+      {
+        field: 'foreignID',
+        selector: '.pl-md-0 a:nth-child(1)',
+        methods: {'attr': ['href']}
+      },
+      // w-100
+      {
+        field: 'imageURL',
+        selector: 'img.w-100',
+        methods: {'attr': ['src'], }
+      },
+    ]
+  },
+  {
     uri: 'https://www.jofogas.hu/szeged?q=kiado%20haz',
     parentSelector: '.box.listing.list-item.reListElement',
     selectors: [
